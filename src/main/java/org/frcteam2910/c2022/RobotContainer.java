cbcd0d66dcc079f75f9af3d1184ee59e1c671727
QuinnP2910 <72775766+QuinnP2910@users.noreply.github.com>
2022-01-27 11:05:30 -0800

    Added Intake

--- src/main/java/org/frcteam2910/c2022/RobotContainer.java
+++ src/main/java/org/frcteam2910/c2022/RobotContainer.java
@@ -1,6 +1,5 @@
 package org.frcteam2910.c2022;
 
-import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -8,9 +7,14 @@ import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import org.frcteam2910.c2022.commands.*;
-import org.frcteam2910.c2022.subsystems.*;
+import org.frcteam2910.c2022.subsystems.FeederSubsystem;
+import org.frcteam2910.c2022.subsystems.IntakeSubsystem;
+import org.frcteam2910.c2022.subsystems.ShooterSubsystem;
+import org.frcteam2910.c2022.commands.DefaultShooterCommand;
 import edu.wpi.first.wpilibj2.command.button.Button;
-import org.frcteam2910.c2022.Robot;
+import org.frcteam2910.c2022.commands.ClimberToPointCommand;
+import org.frcteam2910.c2022.commands.DefaultClimberCommand;
+import org.frcteam2910.c2022.subsystems.ClimberSubsystem;
 
 import java.time.Instant;
 
@@ -20,16 +24,15 @@ public class RobotContainer {
     private final ShooterSubsystem shooter = new ShooterSubsystem();
     private final IntakeSubsystem intake = new IntakeSubsystem();
     private final FeederSubsystem feeder = new FeederSubsystem();
-    private final DrivetrainSubsystem drivetrain = new DrivetrainSubsystem();
 
     private final Joystick joystick = new Joystick(0);
+    private final XboxController controller = new XboxController(Constants.CONTROLLER_PORT);
 
     public RobotContainer() {
         CommandScheduler.getInstance().registerSubsystem(climber);
         CommandScheduler.getInstance().registerSubsystem(shooter);
         CommandScheduler.getInstance().registerSubsystem(intake);
         CommandScheduler.getInstance().registerSubsystem(feeder);
-        CommandScheduler.getInstance().registerSubsystem(drivetrain);
 
         shooter.setDefaultCommand(new DefaultShooterCommand(shooter,
                 () -> joystick.getRawAxis(0) * 12,
@@ -38,14 +41,9 @@ public class RobotContainer {
                 new DefaultIntakeCommand(intake));
         CommandScheduler.getInstance().setDefaultCommand(climber,
                 new DefaultClimberCommand(climber,
-                        () -> joystick.getRawAxis(2),
-                        climber.getPID()
-                ));
-        drivetrain.setDefaultCommand(new DefaultDriveCommand(drivetrain,
-                () -> joystick.getRawAxis(3) * DrivetrainSubsystem.MAX_VELOCITY_METERS_PER_SECOND,
-                () -> joystick.getRawAxis(4) * DrivetrainSubsystem.MAX_VELOCITY_METERS_PER_SECOND,
-                () -> joystick.getRawAxis(5) * DrivetrainSubsystem.MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND
-                ));
+                () -> joystick.getRawAxis(2),
+                climber.getPID()
+        ));
         configureButtonBindings();
     }
 
@@ -54,9 +52,9 @@ public class RobotContainer {
     }
 
     public void configureButtonBindings() {
-        new Button(() -> joystick.getRawButton(1)).whenPressed(new ClimberToPointCommand(climber, 1.0));
-        new Button(() -> joystick.getRawButton(2)).whenPressed(new ClimberToPointCommand(climber, 0.1));
-        // new Button(() -> joystick.getRawButton(3)).whenPressed(new InstantCommand(() -> climber.setPositionControl(false)));
+        new Button(() -> joystick.getRawButton(1)).whileHeld(new ClimberToPointCommand(climber, 1.0));
+        new Button(() -> joystick.getRawButton(2)).whileHeld(new ClimberToPointCommand(climber, 0.1));
+//        new Button(() -> joystick.getRawButton(3)).whenPressed(new InstantCommand(() -> climber.setPositionControl(false)));
         new Button(() -> joystick.getRawButton(4)).whenPressed(new InstantCommand(() -> climber.setPositionControl(true)));
         new Button(() -> joystick.getRawButton(3)).whenPressed(new SequentialCommandGroup(
                 new ClimberToPointCommand(climber, 1.1),
