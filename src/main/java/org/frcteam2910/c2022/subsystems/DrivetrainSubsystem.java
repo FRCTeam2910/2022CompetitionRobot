ec7c1e1fb74f7ff276a0ab8b9fc78e4e50bc7c54
abhyudaitandon <reach.abhyudai@gmail.com>
2022-02-01 21:16:48 -0800

    Added a SwerveDrivePoseEstinator along with documentation for the public methods

--- src/main/java/org/frcteam2910/c2022/subsystems/DrivetrainSubsystem.java
+++ src/main/java/org/frcteam2910/c2022/subsystems/DrivetrainSubsystem.java
@@ -5,6 +5,9 @@ import com.swervedrivespecialties.swervelib.Mk3SwerveModuleHelper;
 import com.swervedrivespecialties.swervelib.Mk4SwerveModuleHelper;
 import com.swervedrivespecialties.swervelib.SdsModuleConfigurations;
 import com.swervedrivespecialties.swervelib.SwerveModule;
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
@@ -20,11 +23,12 @@ import static org.frcteam2910.c2022.Constants.*;
 public class DrivetrainSubsystem extends SubsystemBase {
     public static final double MAX_VOLTAGE = 12.0;
     public static final double MAX_VELOCITY_METERS_PER_SECOND = 6380.0 / 60.0 *
-            SdsModuleConfigurations.MK3_STANDARD.getDriveReduction() *
-            SdsModuleConfigurations.MK3_STANDARD.getWheelDiameter() * Math.PI;
+            SdsModuleConfigurations.MK4_L3.getDriveReduction() *
+            SdsModuleConfigurations.MK4_L3.getWheelDiameter() * Math.PI;
     public static final double MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND = MAX_VELOCITY_METERS_PER_SECOND /
             Math.hypot(DRIVETRAIN_TRACKWIDTH_METERS / 2.0, DRIVETRAIN_WHEELBASE_METERS / 2.0);
-    private final SwerveDriveKinematics m_kinematics = new SwerveDriveKinematics(
+
+    private final SwerveDriveKinematics kinematics = new SwerveDriveKinematics(
             // Front left
             new Translation2d(DRIVETRAIN_TRACKWIDTH_METERS / 2.0, DRIVETRAIN_WHEELBASE_METERS / 2.0),
             // Front right
@@ -34,16 +38,20 @@ public class DrivetrainSubsystem extends SubsystemBase {
             // Back right
             new Translation2d(-DRIVETRAIN_TRACKWIDTH_METERS / 2.0, -DRIVETRAIN_WHEELBASE_METERS / 2.0)
     );
-    private final PigeonIMU m_pigeon = new PigeonIMU(DRIVETRAIN_PIGEON_ID);
-    private final SwerveModule m_frontLeftModule;
-    private final SwerveModule m_frontRightModule;
-    private final SwerveModule m_backLeftModule;
-    private final SwerveModule m_backRightModule;
-    private ChassisSpeeds m_chassisSpeeds = new ChassisSpeeds(0.0, 0.0, 0.0);
+    private final SwerveDrivePoseEstimator estimator;
+
+    private final PigeonIMU pigeon = new PigeonIMU(DRIVETRAIN_PIGEON_ID);
+
+    private final SwerveModule frontLeftModule;
+    private final SwerveModule frontRightModule;
+    private final SwerveModule backLeftModule;
+    private final SwerveModule backRightModule;
+
+    private ChassisSpeeds chassisSpeeds = new ChassisSpeeds(0.0, 0.0, 0.0);
 
     public DrivetrainSubsystem() {
         ShuffleboardTab tab = Shuffleboard.getTab("Drivetrain");
-        m_frontLeftModule = Mk4SwerveModuleHelper.createFalcon500(
+        frontLeftModule = Mk4SwerveModuleHelper.createFalcon500(
                 tab.getLayout("Front Left Module", BuiltInLayouts.kList)
                         .withSize(2, 4)
                         .withPosition(0, 0),
@@ -53,7 +61,7 @@ public class DrivetrainSubsystem extends SubsystemBase {
                 FRONT_LEFT_MODULE_STEER_ENCODER,
                 FRONT_LEFT_MODULE_STEER_OFFSET
         );
-        m_frontRightModule = Mk4SwerveModuleHelper.createFalcon500(
+        frontRightModule = Mk4SwerveModuleHelper.createFalcon500(
                 tab.getLayout("Front Right Module", BuiltInLayouts.kList)
                         .withSize(2, 4)
                         .withPosition(2, 0),
@@ -63,7 +71,7 @@ public class DrivetrainSubsystem extends SubsystemBase {
                 FRONT_RIGHT_MODULE_STEER_ENCODER,
                 FRONT_RIGHT_MODULE_STEER_OFFSET
         );
-        m_backLeftModule = Mk4SwerveModuleHelper.createFalcon500(
+        backLeftModule = Mk4SwerveModuleHelper.createFalcon500(
                 tab.getLayout("Back Left Module", BuiltInLayouts.kList)
                         .withSize(2, 4)
                         .withPosition(4, 0),
@@ -73,7 +81,7 @@ public class DrivetrainSubsystem extends SubsystemBase {
                 BACK_LEFT_MODULE_STEER_ENCODER,
                 BACK_LEFT_MODULE_STEER_OFFSET
         );
-        m_backRightModule = Mk4SwerveModuleHelper.createFalcon500(
+        backRightModule = Mk4SwerveModuleHelper.createFalcon500(
                 tab.getLayout("Back Right Module", BuiltInLayouts.kList)
                         .withSize(2, 4)
                         .withPosition(6, 0),
@@ -83,29 +91,58 @@ public class DrivetrainSubsystem extends SubsystemBase {
                 BACK_RIGHT_MODULE_STEER_ENCODER,
                 BACK_RIGHT_MODULE_STEER_OFFSET
         );
-
+        estimator = new SwerveDrivePoseEstimator(getGyroscopeRotation(), new Pose2d(), kinematics,
+                VecBuilder.fill(0.02, 0.02, 0.01), // estimator values (x, y, rotation) std-devs
+                VecBuilder.fill(0.01), // Gyroscope rotation std-dev
+                VecBuilder.fill(0.1, 0.1, 0.01)); // Vision (x, y, rotation) std-devs
     }
 
-    public void zeroGyroscope() {
-        m_pigeon.setFusedHeading(0.0);
+    private Rotation2d getGyroscopeRotation() {
+        return Rotation2d.fromDegrees(pigeon.getFusedHeading());
     }
-    public double getMaxVelocityMetersPerSecond(){
-        return MAX_VELOCITY_METERS_PER_SECOND;
+
+    /**
+     * Resets the rotation of the drivetrain to zero.
+     */
+    public void zeroRotation() {
+        estimator.resetPosition(new Pose2d(getPose().getX(), getPose().getY(), new Rotation2d()), getGyroscopeRotation());
     }
-    public Rotation2d getGyroscopeRotation() {
-        return Rotation2d.fromDegrees(m_pigeon.getFusedHeading());
+
+    /**
+     * Returns the position of the robot
+     */
+    public Pose2d getPose() {
+        return estimator.getEstimatedPosition();
     }
+
+    /**
+     * Sets the position of the robot to the position passed in with the current gyroscope rotation.
+     */
+    public void setPose(Pose2d pose) {
+        estimator.resetPosition(pose, getGyroscopeRotation());
+    }
+
+    /**
+     * Sets the desired chassis speed of the drivetrain.
+     */
     public void drive(ChassisSpeeds chassisSpeeds) {
-        m_chassisSpeeds = chassisSpeeds;
+        this.chassisSpeeds = chassisSpeeds;
     }
-    public void periodic() {
-        SwerveModuleState[] states = m_kinematics.toSwerveModuleStates(m_chassisSpeeds);
-        SwerveDriveKinematics.desaturateWheelSpeeds(states, MAX_VELOCITY_METERS_PER_SECOND);
 
-        m_frontLeftModule.set(states[0].speedMetersPerSecond / MAX_VELOCITY_METERS_PER_SECOND * MAX_VOLTAGE, states[0].angle.getRadians());
-        m_frontRightModule.set(states[1].speedMetersPerSecond / MAX_VELOCITY_METERS_PER_SECOND * MAX_VOLTAGE, states[1].angle.getRadians());
-        m_backLeftModule.set(states[2].speedMetersPerSecond / MAX_VELOCITY_METERS_PER_SECOND * MAX_VOLTAGE, states[2].angle.getRadians());
-        m_backRightModule.set(states[3].speedMetersPerSecond / MAX_VELOCITY_METERS_PER_SECOND * MAX_VOLTAGE, states[3].angle.getRadians());
+    public void periodic() {
+        SwerveModuleState currentFrontLeftModuleState = new SwerveModuleState(frontLeftModule.getDriveVelocity(), new Rotation2d(frontLeftModule.getSteerAngle()));
+        SwerveModuleState currentFrontRightModuleState = new SwerveModuleState(frontRightModule.getDriveVelocity(), new Rotation2d(frontRightModule.getSteerAngle()));
+        SwerveModuleState currentBackLeftModuleState = new SwerveModuleState(backLeftModule.getDriveVelocity(), new Rotation2d(backLeftModule.getSteerAngle()));
+        SwerveModuleState currentBackRightModuleState = new SwerveModuleState(backRightModule.getDriveVelocity(), new Rotation2d(backRightModule.getSteerAngle()));
+
+        estimator.update(getGyroscopeRotation(), currentFrontLeftModuleState, currentFrontRightModuleState, currentBackLeftModuleState, currentBackRightModuleState);
+
+        SwerveModuleState[] states = kinematics.toSwerveModuleStates(chassisSpeeds);
+        SwerveDriveKinematics.desaturateWheelSpeeds(states, MAX_VELOCITY_METERS_PER_SECOND);
+        frontLeftModule.set(states[0].speedMetersPerSecond / MAX_VELOCITY_METERS_PER_SECOND * MAX_VOLTAGE, states[0].angle.getRadians());
+        frontRightModule.set(states[1].speedMetersPerSecond / MAX_VELOCITY_METERS_PER_SECOND * MAX_VOLTAGE, states[1].angle.getRadians());
+        backLeftModule.set(states[2].speedMetersPerSecond / MAX_VELOCITY_METERS_PER_SECOND * MAX_VOLTAGE, states[2].angle.getRadians());
+        backRightModule.set(states[3].speedMetersPerSecond / MAX_VELOCITY_METERS_PER_SECOND * MAX_VOLTAGE, states[3].angle.getRadians());
     }
 
 }
