d2aefdd1dda1a92e121f1c8ce92f8e6a56d78008
Dapo Nation <72776152+Unknown-Lego@users.noreply.github.com>
2022-02-21 20:43:35 -0800

    Make Default Shooter Command in RPM/Velocity Control

--- src/main/java/org/frcteam2910/c2022/commands/DefaultShooterCommand.java
+++ src/main/java/org/frcteam2910/c2022/commands/DefaultShooterCommand.java
@@ -4,7 +4,7 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import org.frcteam2910.c2022.subsystems.ShooterSubsystem;
 
 public class DefaultShooterCommand extends CommandBase {
-    private static double FLYWHEEL_IDLE_SPEED = 0.1;
+    private static double FLYWHEEL_IDLE_SPEED = 1000;
 
     private final ShooterSubsystem shooter;
 
@@ -14,6 +14,6 @@ public class DefaultShooterCommand extends CommandBase {
     }
 
     public void execute() {
-        shooter.setFlywheelVoltage(FLYWHEEL_IDLE_SPEED);
+        shooter.setTargetFlywheelSpeed(FLYWHEEL_IDLE_SPEED);
     }
 }
