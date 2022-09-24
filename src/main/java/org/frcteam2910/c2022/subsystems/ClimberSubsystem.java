165ce4780d1b47277bb8f4d058d284bb7c602991
QuinnP2910 <72775766+QuinnP2910@users.noreply.github.com>
2022-09-23 17:04:56 -0700

    Revert slow motion constraints to previous values for consistency

--- src/main/java/org/frcteam2910/c2022/subsystems/ClimberSubsystem.java
+++ src/main/java/org/frcteam2910/c2022/subsystems/ClimberSubsystem.java
@@ -50,7 +50,7 @@ public class ClimberSubsystem implements Subsystem {
     private static final MotionProfile.Constraints FAST_MOTION_CONSTRAINTS = new MotionProfile.Constraints(
             Units.feetToMeters(6.0), Units.feetToMeters(12.0));
     private static final MotionProfile.Constraints SLOW_MOTION_CONSTRAINTS = new MotionProfile.Constraints(
-            Units.feetToMeters(4.0), Units.feetToMeters(8.0));
+            Units.feetToMeters(3.0), Units.feetToMeters(6.0));
 
     private final LinearSystem<N2, N1, N1> plant = LinearSystemId.identifyPositionSystem(VELOCITY_CONSTANT,
             ACCELERATION_CONSTANT);
